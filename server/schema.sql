f40f935febfe1a7b1b5ffd6642d434112ee00932
Hack Reactor Students <accounts@hackreactor.com>
2017-02-24 14:42:36 -0600

    Create database schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
 
-
-
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT NOT NULL,
+  message TEXT,
+  id_User INT,
+  id_Room INT,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_User) REFERENCES users(id),
+  FOREIGN KEY(id_Room) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
