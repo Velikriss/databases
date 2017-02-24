e3cfd13ce2ab613365d9eabb57b806745c204a91
Hack Reactor Students <accounts@hackreactor.com>
2017-02-24 16:40:45 -0600

    Post messages to the chat

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,10 @@
 var models = require('../models');
+var Promise = require('bluebird');
 
 module.exports = {
   messages: {
     get: function (request, response) {
-      models.messages.get(results => {
+      models.messages.get((error, results, fields) => {
         response.writeHead(200, {'Content-Type': 'application/json'});
         
         var messages = results.map(row => (
@@ -20,7 +21,17 @@ module.exports = {
       });
 
     }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (request, response) {
+      var parameters = {
+        message: request.body.text,
+        user: request.body.username,
+        room: request.body.roomname
+      };
+      models.messages.post( parameters, (error, results, fields) => {
+        response.writeHead(201, {'Content-Type': 'application/json'});
+        response.end();
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,21 @@
 var db = require('../db');
 var mysql = require('mysql');
+var Promise = require('bluebird');
 
 module.exports = {
   messages: {
     get: function (callback) {
-      db.query('SELECT * FROM messages', (error, results, fields) => {
-        console.log(results);
-        callback(results);
-      });
+      db.query('SELECT * FROM messages', callback);
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (parameters, callback) {
+      db.query(
+        mysql.format(
+          'INSERT INTO messages (message, user, room) VALUES (?, ?, ?)',
+          [parameters.message, parameters.user, parameters.room]
+        ),
+        callback
+      );
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
